2f7105963c8e454c8b55464331346402b84b7582
ishii yuka <xx2s2xx2@gmail.com>
2023-02-03 18:44:10 +0900

    コミット3

--- jquery.js
+++ jquery.js
@@ -1,10 +1,25 @@
 
 $(function(){
 
-//tabのpic表示
-$(".tab-list li").on("click", function () {
+  //tabのpic表示
+  $(".tab-list li").on("click", function () {
     let id = $(this).attr("id"); //attra(classとかtypeとかdiv)
     console.log($(this).attr("id"));
 
-})
+  })
+
+  // island-menu アコーディオン
+  $("#island").on("click",function(){
+    $(this).siblings().slideToggle();
+    console.log(this);
+  })
+
+
+
+  // button トップへ戻る
+  $(".go-top-btn").on("click",function(){
+    $("body,html").animate({scrollTop:0},500);
+  })  
+
+
 })//最後
