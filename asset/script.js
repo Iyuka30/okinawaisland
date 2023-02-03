adf17a6059f9387599107f907bf17fdff5815e7e
DESKTOP-J3PPFPG\harux <haru.xoxo7@gmail.com>
2023-02-03 17:13:23 +0900

    0203コミット

--- asset/script.js
+++ asset/script.js
@@ -2,11 +2,11 @@ $(function () {
 
     //tabのpic表示
   
-    
+
     //top戻る
     $(".go-top-btn").on("click", function () {
         $("body,html").animate({ scrollTop: 0 }, 500);
         console.log("go-top-btn");
-    })
+    });
 
 })//最後
