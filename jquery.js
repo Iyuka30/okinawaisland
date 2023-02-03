d6d7a4f75c1ed740c2200ac5cf2a5941fbde2ce5
DESKTOP-J3PPFPG\harux <haru.xoxo7@gmail.com>
2023-02-03 19:26:28 +0900

    0203 -4

--- jquery.js
+++ jquery.js
@@ -14,7 +14,36 @@ $(function(){
     console.log(this);
   })
 
+  // コメント入力したときadd押すと次のサイトに飛べる
 
+  $("#hontou").on("click", function () {
+    console.log($("#hontou"));
+    window.location.href = 'https://www.okinawastory.jp/';
+  })
+  $("#yonaguni").on("click", function () {
+    console.log($("#yonaguni"));
+    window.location.href = 'https://welcome-yonaguni.jp/';
+  })
+
+  $("#ishigaki").on("click", function () {
+    console.log($("#ishigaki"));
+    window.location.href = '  https://ooritoori-ishigaki.com/';
+  })
+
+  $("#iriomote").on("click", function () {
+    console.log($("#iriomote"));
+    window.location.href = ' https://painusima.com/category/sima/iriomotejimawest/';
+  })
+
+  $("#miyakojima").on("click", function () {
+    console.log($("#miyakojima"));
+    window.location.href = ' https://miyako-guide.net/';
+  })
+
+  $("#kumejima").on("click", function () {
+    console.log($("#kumejima"));
+    window.location.href = 'https://www.kanko-kumejima.com/know';
+  })
 
   // button トップへ戻る
   $(".go-top-btn").on("click",function(){
